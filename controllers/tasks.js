a319b866110cd29242bda46920f78bb127d0b62c
tayyab-ilyas <tayyabilyas963@gmail.com>
2023-11-25 22:50:05 +0530

    basic methods setup

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,23 @@
 const getAllTasks = (req,res)=>{
     res.send('all items')
 }
+const createTask = (req, res)=>{
+    res.send('create task');
+}
+const getTask = (req,res)=>{
+    res.send('get single task');
+}
+const updateTask = (req,res)=>{
+    res.send('update tasks');
+}
+const deleteTask = (req,res)=>{
+    res.send('delete task');
+}
 
 module.exports = {
     getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 }
