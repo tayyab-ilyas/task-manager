a4156716942c894666550eb3f33c4d3510cf85a3
tayyab-ilyas <tayyabilyas963@gmail.com>
2023-11-25 22:42:31 +0530

    get controller

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
+
+
 
 //routes
-app.get('/',(req, res)=>{
-    res.send('Task Manager App')
-})
+app.use('/api/v1/tasks', tasks);
 // app.get('/api/v1/tasks') ------ get all tasks
 // app.post('/api/v1/tasks') ----- create a new task
 // app.get('/api/v1/tasks/:id') ----- get single task
