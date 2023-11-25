f9f463eacfe8e106b90ba82589e4c9287b6b3ab8
tayyab-ilyas <tayyabilyas963@gmail.com>
2023-11-25 22:24:41 +0530

    api structure

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+//routes
+app.get('/',(req, res)=>{
+    res.send('Task Manager App')
+})
+// app.get('/api/v1/tasks') ------ get all tasks
+// app.post('/api/v1/tasks') ----- create a new task
+// app.get('/api/v1/tasks/:id') ----- get single task
+// app.patch('/api/v1/tasks/:id') ----- update task
+// app.delete('api/v1/tasks/:id') ----- delete task
+
 const port = 5000;
 
 app.listen(port,()=>{
