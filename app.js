b7384b4196b13867e6619f6f88384b4314ac8214
tayyab-ilyas <tayyabilyas963@gmail.com>
2023-11-27 18:38:24 +0530

    add middlewares

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middlewares/not-found");
 require("dotenv").config();
-
 //middlewares
 app.use(express.static("./public"));
 app.use(express.json());
 
 //routes
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
 // app.get('/api/v1/tasks') ------ get all tasks
 // app.post('/api/v1/tasks') ----- create a new task
 // app.get('/api/v1/tasks/:id') ----- get single task
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,9 @@
 const Task = require("../models/Task");
+
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(200).json({ tasks });
+    res.status(200).json({ status: "success", nbHits: tasks.length });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
