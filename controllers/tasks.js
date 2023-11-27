fb58fc4818b987f2890228ade18a1917ce57089a
tayyab-ilyas <tayyabilyas963@gmail.com>
2023-11-27 17:01:43 +0530

    create schema

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,17 @@
+const Task = require("../models/Task");
 const getAllTasks = (req, res) => {
   res.send("get all tasks");
 };
-const createTask = (req, res) => {
-  res.send("create task");
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const getTask = (req, res) => {
-  res.send("get single task");
+  res.json({ id: req.params.id });
 };
 const updateTask = (req, res) => {
   res.send("update tasks");
